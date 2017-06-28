99310c41197ccf581338fa6f9a6a60ee05d53ad6
Van Roye <steven.van-roye@sogeti.com>
2017-06-28 22:39:34 +0200

    Second commit

--- js/map.js
+++ js/map.js
@@ -1,80 +1,83 @@
  var map;
 
-      // Create a new blank array for all the listing markers.
-      var markers = [];
+// Create a new blank array for all the listing markers.
+var markers = [];
 
-      // Create placemarkers array to use in multiple functions to have control
-      // over the number of places that show.
-      var placeMarkers = [];
+// Create placemarkers array to use in multiple functions to have control
+// over the number of places that show.
+var placeMarkers = [];
 
-      function initMap() {
+var scriptLoaded = false;
+
+function initMap() {
+  scriptLoaded = true;
         // Create a styles array to use with the map.
         var styles = [
-          {
-            featureType: 'water',
-            stylers: [
-              { color: '#19a0d8' }
-            ]
-          },{
-            featureType: 'administrative',
-            elementType: 'labels.text.stroke',
-            stylers: [
-              { color: '#ffffff' },
-              { weight: 6 }
-            ]
-          },{
-            featureType: 'administrative',
-            elementType: 'labels.text.fill',
-            stylers: [
-              { color: '#e85113' }
-            ]
-          },{
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [
-              { color: '#efe9e4' },
-              { lightness: -40 }
-            ]
-          },{
-            featureType: 'transit.station',
-            stylers: [
-              { weight: 9 },
-              { hue: '#e85113' }
-            ]
-          },{
-            featureType: 'road.highway',
-            elementType: 'labels.icon',
-            stylers: [
-              { visibility: 'off' }
-            ]
-          },{
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [
-              { lightness: 100 }
-            ]
-          },{
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [
-              { lightness: -100 }
-            ]
-          },{
-            featureType: 'poi',
-            elementType: 'geometry',
-            stylers: [
-              { visibility: 'on' },
-              { color: '#f0e4d3' }
-            ]
-          },{
-            featureType: 'road.highway',
-            elementType: 'geometry.fill',
-            stylers: [
-              { color: '#efe9e4' },
-              { lightness: -25 }
-            ]
-          }
-        ];
+        { 
+          featureType: 'water',
+          stylers: [
+          { color: '#19a0d8' }
+          ]
+        },{
+         featureType: 'administrative',
+         elementType: 'labels.text.stroke',
+         stylers: [
+         { color: '#ffffff' },
+         { weight: 6 }
+         ]
+       },{
+        featureType: 'administrative',
+        elementType: 'labels.text.fill',
+        stylers: [
+        { color: '#e85113' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [
+        { color: '#efe9e4' },
+        { lightness: -40 }
+        ]
+      },{
+        featureType: 'transit.station',
+        stylers: [
+        { weight: 9 },
+        { hue: '#e85113' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'labels.icon',
+        stylers: [
+        { visibility: 'off' }
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [
+        { lightness: 100 }
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+        { lightness: -100 }
+        ]
+      },{
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+        { visibility: 'on' },
+        { color: '#f0e4d3' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [
+        { color: '#efe9e4' },
+        { lightness: -25 }
+        ]
+      }
+      ];
 
         // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
@@ -84,33 +87,12 @@
           mapTypeControl: false
         });
 
-        /*
-        // This autocomplete is for use in the search within time entry box.
-        var timeAutocomplete = new google.maps.places.Autocomplete(
-            document.getElementById('search-within-time-text'));
-        // This autocomplete is for use in the geocoder entry box.
-        var zoomAutocomplete = new google.maps.places.Autocomplete(
-            document.getElementById('zoom-to-area-text'));
-        // Bias the boundaries within the map for the zoom to area text.
-        zoomAutocomplete.bindTo('bounds', map);
-        // Create a searchbox in order to execute a places search
-        var searchBox = new google.maps.places.SearchBox(
-            document.getElementById('places-search'));
-        // Bias the searchbox to within the bounds of the map.
-        searchBox.setBounds(map.getBounds());
+/*
 
-        // These are the real estate listings that will be shown to the user.
-        // Normally we'd have these in a database instead.
-        var locations = [
-          {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-          {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-          {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-          {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-          {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-        ];
-
-        var largeInfowindow = new google.maps.InfoWindow();
+        var placeAutocomplete = new google.maps.places.Autocomplete(
+          document.getElementById('search-place-text'));
+        
+        
 
         // Style the markers a bit. This will be our listing marker icon.
         var defaultIcon = makeMarkerIcon('0091ff');
@@ -173,9 +155,10 @@
         document.getElementById('go-places').addEventListener('click', textSearchPlaces);
         */
 
+        ko.applyBindings(new AppViewModel());
       }
 
-/*
+
       // This function populates the infowindow when the marker is clicked. We'll only allow
       // one infowindow which will open at the marker that is clicked, and populate based
       // on that markers position.
@@ -199,14 +182,14 @@
               var nearStreetViewLocation = data.location.latLng;
               var heading = google.maps.geometry.spherical.computeHeading(
                 nearStreetViewLocation, marker.position);
-                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-                var panoramaOptions = {
-                  position: nearStreetViewLocation,
-                  pov: {
-                    heading: heading,
-                    pitch: 30
-                  }
-                };
+              infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+              var panoramaOptions = {
+                position: nearStreetViewLocation,
+                pov: {
+                  heading: heading,
+                  pitch: 30
+                }
+              };
               var panorama = new google.maps.StreetViewPanorama(
                 document.getElementById('pano'), panoramaOptions);
             } else {
@@ -222,17 +205,9 @@
         }
       }
 
-      // This function will loop through the markers array and display them all.
-      function showListings() {
-        var bounds = new google.maps.LatLngBounds();
-        // Extend the boundaries of the map for each marker and display the marker
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-          bounds.extend(markers[i].position);
-        }
-        map.fitBounds(bounds);
-      }
 
+
+/*
       // This function will loop through the listings and hide them all.
       function hideMarkers(markers) {
         for (var i = 0; i < markers.length; i++) {
@@ -522,3 +497,4 @@
     }
 
     */
+
--- js/script.js
+++ js/script.js
@@ -1,79 +1,131 @@
 var importantPlaces = [
 {
-	name: "Flanders Expo",
-	address: "Maaltekouter 1, 9051 Gent",
-	location: {lat: 51.025984 , lng: 3.690763}
+  name: "Flanders Expo",
+  address: "Maaltekouter 1, 9051 Gent",
+  location: {lat: 51.025984 , lng: 3.690763},
+  showItem: true
 },
 {
-	name: "Vooruit",
-	address: "Sint-Pietersnieuwstraat 23, 9000 Gent",
-	location: {lat: 51.047704 , lng: 3.727382}
+  name: "Vooruit",
+  address: "Sint-Pietersnieuwstraat 23, 9000 Gent",
+  location: {lat: 51.047704 , lng: 3.727382},
+  showItem: true
 },
 {
-	name: "Stedelijk Museum voor Actuele Kunst",
-	address: "Jan Hoetplein 1, 9000 Gent",
-	location: {lat: 51.037257 , lng: 3.723533}
+  name: "Stedelijk Museum voor Actuele Kunst",
+  address: "Jan Hoetplein 1, 9000 Gent",
+  location: {lat: 51.037257 , lng: 3.723533},
+  showItem: true
 },
 {
-	name: "Capitool",
-	address: "Graaf Van Vlaanderenplein 5, 9000 Gent",
-	location: {lat: 51.04811 , lng: 3.732215}
+  name: "Capitool",
+  address: "Graaf Van Vlaanderenplein 5, 9000 Gent",
+  location: {lat: 51.04811 , lng: 3.732215},
+  showItem: true
 },
 {
-	name: "Sint-Baafskathedraal",
-	address: "Sint-Baafsplein, 9000 Gent",
-	location: {lat: 51.05339 , lng: 3.725113}
+  name: "Sint-Baafskathedraal",
+  address: "Sint-Baafsplein, 9000 Gent",
+  location: {lat: 51.05339 , lng: 3.725113},
+  showItem: true
 },
 {
-	name: "Korenmarkt",
-	address: "Korenmarkt 18, 9000 Gent",
-	location: {lat: 51.054627 , lng: 3.721769}
+  name: "Korenmarkt",
+  address: "Korenmarkt 18, 9000 Gent",
+  location: {lat: 51.054627 , lng: 3.721769},
+  showItem: true
 },
 {
-	name: "Gravensteen",
-	address: "Sint-Veerleplein 11, 9000 Gent",
-	location: {lat: 51.057529 , lng: 3.720737}
+  name: "Gravensteen",
+  address: "Sint-Veerleplein 11, 9000 Gent",
+  location: {lat: 51.057529 , lng: 3.720737},
+  showItem: true
 },
 {
-	name: "Kinepolis",
-	address: "Ter Platen 12, 9000 Gent",
-	location: {lat: 51.040544 , lng: 3.729963}
+  name: "Kinepolis",
+  address: "Ter Platen 12, 9000 Gent",
+  location: {lat: 51.040544 , lng: 3.729963},
+  showItem: true
 },
 {
-	name: "Gent-Sint-Pietersstation",
-	address: "Koningin Maria Hendrikaplein 1, 9000 Gent",
-	location: {lat: 51.03725 , lng: 3.709497}
+  name: "Gent-Sint-Pietersstation",
+  address: "Koningin Maria Hendrikaplein 1, 9000 Gent",
+  location: {lat: 51.03725 , lng: 3.709497},
+  showItem: true
 },
 {
-	name: "Universitair Ziekenhuis Gent",
-	address: "De Pintelaan 185, 9000 Gent",
-	location: {lat: 51.024557 , lng: 3.725222}
+  name: "Universitair Ziekenhuis Gent",
+  address: "De Pintelaan 185, 9000 Gent",
+  location: {lat: 51.024557 , lng: 3.725222},
+  showItem: true
 }
-
 ];
 
 var Place = function(data){
-	this.name = data.name;
-	this.address = data.address;
+  this.name = data.name;
+  this.address = data.address;
+  this.location = data.location;
+  this.marker = data.marker;
+  this.showItem = data.showItem;
 };
 
 function AppViewModel() {
-	var self = this;
-	this.placesList = ko.observableArray();
-	this.markersList = ko.observableArray();
+  var self = this;
+  this.placesList = ko.observableArray();
+  this.markersList = ko.observableArray();
 
-	importantPlaces.forEach(function(item,number){
-		self.placesList.push(new Place(item));
-		var marker = new google.maps.Marker({
-            position: item.location,
-            title: item.name,
-            animation: google.maps.Animation.DROP,
-            id: number
-          });
-	})
+  var largeInfowindow = new google.maps.InfoWindow();
+  var inputField = document.getElementById('search-place-text');
+
+  importantPlaces.forEach(function(item,number){
+   
+    var marker = new google.maps.Marker({
+      position: item.location,
+      title: item.name,
+      animation: google.maps.Animation.DROP,
+      id: number
+    });
+
+    self.markersList.push(marker);
+
+    item.marker = marker;
+    
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+
+    self.placesList.push(new Place(item));
+  })
+
+  this.placeName = ko.observable("");
+
+
+  this.updateList = function() {
+    var currentValue = self.placeName();
+    for(var i=0; i< self.placesList().length;i++){
+      if(!self.placesList()[i].name.includes(currentValue) && !self.placesList()[i].address.includes(currentValue)){
+        self.placesList()[i].showItem = false;
+      } 
+    }
+  }
+
+  var bounds = new google.maps.LatLngBounds();
+  this.markersList().forEach(function(item, number){
+    item.setMap(map);
+    bounds.extend(item.position);
+  })
+  map.fitBounds(bounds);
+
+  this.openMarker = function(item){
+    populateInfoWindow(item.marker,largeInfowindow);
+  }
 
 
 }
 
+document.onerror = function() {
+  alert('Problem with loading the script. Please check your connetivity and try again later.')
+}
 
-ko.applyBindings(new AppViewModel());
+
+
