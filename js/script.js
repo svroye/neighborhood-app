d038ecc89fdfa2352c6042420065375be619edae
Van Roye <steven.van-roye@sogeti.com>
2017-06-29 21:35:56 +0200

    Finish filter feature

--- js/script.js
+++ js/script.js
@@ -72,15 +72,18 @@ var Place = function(data){
 function AppViewModel() {
 
   var self = this;
+  //create array for the place items
   this.placesList = ko.observableArray();
-  this.markersList = ko.observableArray();
-
+  //create variables for making an infoWindow for the markers and for the bounds of the map
   var largeInfowindow = new google.maps.InfoWindow();
-  var inputField = document.getElementById('search-place-text');
   var bounds = new google.maps.LatLngBounds();
 
+  // loop through the places, make a marker for each of them, extend the bounds of the map to
+  // include the place, add a listener for the marker to fill it when clicked
+  // Also create a knockout observable for each place and add it to the array defined above 
   importantPlaces.forEach(function(item,number){
 
+    //create the marker
     var marker = new google.maps.Marker({
       position: item.location,
       title: item.name,
@@ -89,12 +92,15 @@ function AppViewModel() {
       map: map
     });
 
+    // extend the boundaries of the map
     bounds.extend(marker.position);
 
+    // add clicklistener for filling and opening the infowindow 
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
     });
 
+    // create a new variable for the places
     self.newItem = ko.observable({
       name: item.name,
       address: item.address,
@@ -103,29 +109,51 @@ function AppViewModel() {
       showItem: true
     });
 
+    // add the variable to the array
     self.placesList.push(self.newItem)
   })
 
+  // variable for the input value in the filterbox
   this.placeName = ko.observable("");
 
 
-
+  // called whenever the user types something in the filter box
   this.updateList = function() {
+    //takes current value in the filter box
     var currentValue = self.placeName().toLowerCase();
-    console.log(currentValue)
-
+    // loop through the different placesin the array
     for(var i=0; i< self.placesList().length;i++){
+      // take the current element of the array and its values
       var currentElement = self.placesList()[i];
-      var currentAddress = currentElement().address;
       var currentName = currentElement().name;
+      var currentAddress = currentElement().address;
+      var currentLocation = currentElement().location;
+      var currentMarker = currentElement().marker;
+      var currentShowItem = currentElement().showItem;
       
-      if(!currentName.includes(currentValue) && !currentAddress.includes(currentValue)){
-        currentElement().showItem = false;
-      } 
+      if(!currentName.toLowerCase().includes(currentValue) && !currentAddress.toLowerCase().includes(currentValue)){
+        currentElement({
+          name: currentName,
+          address: currentAddress,
+          location: currentLocation,
+          marker: currentMarker,
+          showItem: false
+        })
+        currentMarker.setMap(null);
+      } else {
+        currentElement({
+          name: currentName,
+          address: currentAddress,
+          location: currentLocation,
+          marker: currentMarker,
+          showItem: true
+        })
+        currentMarker.setMap(map);
+      }
     }
   }
   
-
+  // called when a list item is clicked
   this.openMarker = function(item){
     populateInfoWindow(item.marker,largeInfowindow);
   }
